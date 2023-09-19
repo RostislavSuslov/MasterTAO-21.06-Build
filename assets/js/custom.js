327e931a2f0d1abe565bd18a4e7e335672126c79
RostSuslov <suslovros@gmail.com>
2023-09-19 19:50:56 +0300

    first

---
+++ assets/js/custom.js
@@ -0,0 +1,58 @@
+const burger = document.querySelector('#hamburger');
+
+burger.addEventListener('click', function() {
+    burger.classList.toggle('is-active')
+})
+
+document.getElementById('fileInput').onchange = function () {
+  
+    //short name
+    // document.getElementById('file-name').innerHTML = this.files[0].name;
+    
+    //long name
+    document.getElementById('file-name').innerHTML = this.value;
+};
+
+$(document).ready(function() {
+    $('.first-screen').slick({
+        infinite: true,
+        dots: true,
+        appendArrows: '.first-screen-nav',
+        responsive: [
+            {
+              breakpoint: 991,
+              settings: {
+                arrows: false,
+              }
+            },
+          ]
+    });
+    $('.first-screen').append($('.first-screen-nav'));
+
+
+    mobileOnlySlider("#advanages-slider", true, false, 991);
+
+    function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+        var slider = $($slidername);
+        var settings = {
+            mobileFirst: true,
+            dots: $dots,
+            arrows: $arrows,
+            responsive: [{
+                breakpoint: $breakpoint,
+                settings: "unslick"
+            }]
+        };
+
+        slider.slick(settings);
+
+        $(window).on("resize", function() {
+            if ($(window).width() > $breakpoint) {
+                return;
+            }
+            if (!slider.hasClass("slick-initialized")) {
+                return slider.slick(settings);
+            }
+        });
+    } // Mobile Only Slider
+});
